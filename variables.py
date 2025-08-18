ed100cd61c8501fa1f837ff91098d189d4036ad2
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-08-19 00:54:40 +0100

    Add: Initial implementation of app.py and variables.py with basic print statements and variable examples

---
+++ variables.py
@@ -0,0 +1,61 @@
+# variables
+number = 5
+name = "john"
+
+print(number)
+print(name)
+
+
+# variable casting
+x = str(3)
+y = int(10)
+z = float(50)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+myName = "Muhammad"
+print(myName)
+
+my_var = "Muhammad"
+print(my_var)
+
+_my_var = "Muhammad"
+print(_my_var)
+
+
+x = 1
+
+
+# multiple variables
+fruit, card, number = "apple", "king", 5
+
+print(fruit)
+print(card)
+print(number)
+
+
+# output multiple variables
+
+lang = "Python "
+y = "is "
+z = "awesome"
+
+print(lang + y + z)
+print(lang, y, z)
+
+
+num = 5 
+string = "desk"
+
+# print(num + string)   TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+print(num, string)
+
+
