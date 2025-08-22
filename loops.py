de21062d2085aa9d1cd56784729a3c0b63d95505
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-08-22 09:12:35 +0100

    Add: Create loops.py to demonstrate various loop constructs including for loops, break, continue, and pass statements

---
+++ loops.py
@@ -0,0 +1,73 @@
+
+
+# For Loops
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+    print(x) 
+
+
+for number in range(3):
+    print("Python", number, number * ".")
+
+
+for number in range(1, 5):
+    print("Attempting", number, number * ".")
+
+
+# third argument as step 
+for number in range(1, 10, 2):
+    print("Attempting", number, number * ".")
+
+
+# breaking out of a loop
+
+# successful = True
+successful = False
+for number in range(3):
+    print("Sending")
+    if successful:
+        print("Attempt Successful")
+        break
+    else: 
+        print("Attempted 3 times and failed")
+
+
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
+
+
+fruits = ["apple", "banana", "cherry", "mango", "pineapple"]
+
+for fruit in fruits:
+    print(fruit)
+
+for fruit in fruits:
+    print(fruit)
+    if fruit == "banana":
+        # print("banana is present")
+        break
+
+
+# The continue Statement
+products = ["shoe", "bags", "chair", "table"]
+
+for product in products:
+    if product == "bags":
+        continue
+    print(product)
+
+
+for number in range(2, 5):
+    print(number) # 2-4
+
+
+for number in range(2, 30, 3):
+    print(number)  #2, 5, 8 - 29
+
+
+# The pass Statement
+
+for x in [0, 1, 2, 3, 4, 5]:
+    pass
