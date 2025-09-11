c976483985a1f9c5ad968ed0073f11ae6a46408b
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-09-12 00:44:21 +0100

    Add: Include platform module usage to display system information and defined names

--- modules/use_math_operations.py
+++ modules/use_math_operations.py
@@ -1,4 +1,5 @@
 import math_operations as math_op
+import platform
 
 num1 = 10
 num2 = 5
@@ -9,3 +10,13 @@ print("Subtraction", math_op.subtract(num1, num2))
 print("Multiplication", math_op.multiply(num1, num2))
 print("Division", math_op.divide(num1, num2))
 
+
+
+x = platform.system()
+print(x)
+
+
+# List all the defined names belonging to the platform module:
+
+dir = dir(platform)
+print(dir)
