9fbddddcdbc0a6ed2c5d69791cd6d59671383c02
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-09-15 12:07:41 +0100

    update: quiz

---
+++ pythonic_code.py
@@ -0,0 +1,12 @@
+# Pythonic codes
+# Pythonic code means writing code that follows the style, idioms, and conventions of Python — code that feels natural to Python programmers.
+
+
+# [expression for item in interable if condition]
+# expression → what you want to compute/store for each item
+
+# item → variable representing each element in the iterable
+
+# iterable → any sequence (list, tuple, string, range, etc.)
+
+# if condition → optional filter (only include items that satisfy this condition)
