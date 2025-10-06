1713caa4c20b241845399a162619fe2c327fc667
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-10-06 16:02:10 +0100

    add: static method

---
+++ oop/static_method.py
@@ -0,0 +1,22 @@
+class Calculator:
+    base_value = 100
+
+    @staticmethod
+    def add(value1, value2):
+        return value1 + value2
+
+    @staticmethod
+    def subtract(value1, value2):
+        return value1 - value2
+    
+    @classmethod
+    def multiply_base(cls, multiplier):
+        return cls.base_value * multiplier
+    
+
+# Using Static Method
+print(Calculator.add(4, 5))
+
+print(Calculator.subtract(4, 5))
+
+print(Calculator.multiply_base(2))
